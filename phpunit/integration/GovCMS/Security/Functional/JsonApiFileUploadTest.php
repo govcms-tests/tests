a1154ce5d1d0fa4b8e5a899e5f6f9e1b6cf972bf
cb-govcms <colin.bernays@finance.gov.au>
2025-09-04 10:32:53 +1000

    update tests to dynamic JSON:API version

--- phpunit/integration/GovCMS/Security/Functional/JsonApiFileUploadTest.php
+++ phpunit/integration/GovCMS/Security/Functional/JsonApiFileUploadTest.php
@@ -15,6 +15,7 @@ use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\file\Entity\File;
 use Drupal\govcms_security\GovcmsFileConstraintInterface;
+use Drupal\jsonapi\JsonApiSpec;
 use Drupal\user\RoleInterface;
 use Drupal\user\UserInterface;
 use Drupal\user\Entity\Role;
@@ -281,10 +282,10 @@ class JsonApiFileUploadTest extends BrowserTestBase {
       'jsonapi' => [
         'meta' => [
           'links' => [
-            'self' => ['href' => 'http://jsonapi.org/format/1.0/'],
+            'self' => ['href' => JsonApiSpec::SUPPORTED_SPECIFICATION_PERMALINK],
           ],
         ],
-        'version' => '1.0',
+        'version' => JsonApiSpec::SUPPORTED_SPECIFICATION_VERSION,
       ],
       'links' => [
         'self' => ['href' => $self_url],
